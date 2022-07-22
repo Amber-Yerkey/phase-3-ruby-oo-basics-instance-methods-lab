664f17bfe304207dd81851aa47dbde7a2fe083b0
Amber Frantz <amber.k.frantz@mgail.com>
2022-07-22 09:59:17 -0400

    Completed Assignment

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+
+class Person
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+# amber = Person.new
+# amber.talk
+# amber.walk
